d9887fd3422f21c200635601612910f436f300ee
yagiz <yagocevat123@gmail.com>
2026-07-02 16:47:15 +0300

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header/Header.jsx';
+import Hero from './components/Hero/Hero.jsx';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="text-white">Hero Section</div>
+    </div>
+  )
+}
+
+export default Hero
